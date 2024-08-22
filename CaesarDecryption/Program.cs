be56714656af113b6cb1dbc63019e1fae6b65e88
WiteDeaHub <witedea@gmail.com>
2024-08-22 15:57:24 +0300

    Update Program.cs

--- CaesarDecryption/Program.cs
+++ CaesarDecryption/Program.cs
@@ -1,5 +1,5 @@
 ﻿char[] alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ".ToCharArray();    //Enter letters of the alphabet in this field
-char[] cipherText = "AçıkMetin".ToCharArray();  //Enter cipher text in this field
+char[] cipherText = "ŞifreliMetin".ToCharArray();  //Enter cipher text in this field
 int key = 3;    //enter the key in this field
 string plainText = string.Empty;
 
