65d389f314a1ded36d7ab1ff0b24470952280d13
wited <wited@LAPTOP-CO67K0CT>
2024-08-22 15:45:00 +0300

    Proje dosyası ekle.

---
+++ CaesarDecryption/Program.cs
@@ -0,0 +1,12 @@
+﻿char[] alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ".ToCharArray();    //Enter letters of the alphabet in this field
+char[] cipherText = "AçıkMetin".ToCharArray();  //Enter cipher text in this field
+int key = 3;    //enter the key in this field
+string plainText = string.Empty;
+
+for (int i = 0; i < cipherText.Length; i++)
+{
+    int ndx = (Array.IndexOf(alphabet, char.ToUpper(cipherText[i])) - key);
+    while (ndx < 0) ndx += alphabet.Length;
+    plainText += alphabet[ndx];
+}
+Console.WriteLine(plainText);
